034fbee1cd7c63513b3eaf1cc98f2fc9972d966a
Juan Fernandez-Corugedo <juan.fcorugedo@sngular.team>
2016-05-19 13:59:39 +0200

    adds swagger configuration

---
+++ src/main/java/com/jfcorugedo/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.jfcorugedo.config;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Profile("!integrationtest")
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket yoigoApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+					.groupName("restApi")
+					.apiInfo(apiInfo())
+					.select()
+					.paths(regex("/v1.*"))
+					.build();
+					
+	}
+
+	private ApiInfo apiInfo() {
+		
+		return new ApiInfoBuilder()
+				.title("REST API")
+				.description("Demo REST API using Spring Boot")
+				.contact(new Contact("jfcorugedo", "https://es.linkedin.com/in/jfcorugedo", "juan.fcorugedo@sngular.team"))
+				.license("The Unlicense")
+				.licenseUrl("http://unlicense.org/")
+				.build();
+	}
+}
--- src/test/java/com/jfcorugedo/RestApiApplicationTests.java
+++ src/test/java/com/jfcorugedo/RestApiApplicationTests.java
@@ -3,12 +3,14 @@ package com.jfcorugedo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RestApiApplication.class)
 @WebAppConfiguration
+@ActiveProfiles("integrationtest")
 public class RestApiApplicationTests {
 
 	@Test
