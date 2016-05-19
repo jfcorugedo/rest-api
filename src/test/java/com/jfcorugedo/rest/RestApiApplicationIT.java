4f6d936a5c99e8af31009609143dab79c46d3600
Juan Fernandez-Corugedo <juan.fcorugedo@sngular.team>
2016-05-19 17:59:59 +0200

    improves tests speed changing spring boot annotation so this test and mobile conroller test use the same spring context in their test

--- src/test/java/com/jfcorugedo/rest/RestApiApplicationIT.java
+++ src/test/java/com/jfcorugedo/rest/RestApiApplicationIT.java
@@ -3,15 +3,13 @@ package com.jfcorugedo.rest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-import com.jfcorugedo.rest.RestApiApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RestApiApplication.class)
-@WebAppConfiguration
+@WebIntegrationTest("server.port:0")
 @ActiveProfiles("integrationtest")
 public class RestApiApplicationIT {
 
