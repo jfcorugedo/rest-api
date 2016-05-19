fc49385356f8ae4f5ea3d006953e50c5c2dbeee8
Juan Fernandez-Corugedo <juan.fcorugedo@sngular.team>
2016-05-19 14:16:56 +0200

    adds swagger documentation annotations

--- src/main/java/com/jfcorugedo/mobile/MobileRestController.java
+++ src/main/java/com/jfcorugedo/mobile/MobileRestController.java
@@ -1,19 +1,31 @@
 package com.jfcorugedo.mobile;
 
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/v1")
 public class MobileRestController {
 
-	@RequestMapping("/mobiles/{id}")
-	public ResponseEntity<Mobile> findOne(@PathVariable String id) {
+	@RequestMapping(path="/mobiles/{id}", method=GET)
+	@ApiOperation(value="Returns the mobile with the given ID", produces="application/json")
+	@ApiResponses(value = {
+			@ApiResponse(code=200, message="Succcess"),
+			@ApiResponse(code=404, message="Not found"),
+	})
+	public ResponseEntity<Mobile> getMobile(@PathVariable String id) {
 		
 		Mobile mobile = new Mobile()
+							.setId("126")
 							.setName("IPhone 6S")
 							.setBrand("Apple")
 							.setColor("Silver")
